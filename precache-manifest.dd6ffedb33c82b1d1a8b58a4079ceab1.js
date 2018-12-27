ea7ceef8d6dc7071fd8470dbda282d5c798f3d6d
taiyoslime <hase.tekitou@gmail.com>
2018-12-27 18:38:30 +0900

    Updates

---
+++ precache-manifest.dd6ffedb33c82b1d1a8b58a4079ceab1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a467243b6eb10c195d03",
+    "url": "/shitposting/static/js/runtime~main.a467243b.js"
+  },
+  {
+    "revision": "18573349eebbee820dc4",
+    "url": "/shitposting/static/js/main.18573349.chunk.js"
+  },
+  {
+    "revision": "bd5c2da9c17548fa2999",
+    "url": "/shitposting/static/js/1.bd5c2da9.chunk.js"
+  },
+  {
+    "revision": "18573349eebbee820dc4",
+    "url": "/shitposting/static/css/main.06cf7629.chunk.css"
+  },
+  {
+    "revision": "998a2bf222bc33060c7b23b2323e0cd0",
+    "url": "/shitposting/index.html"
+  }
+];

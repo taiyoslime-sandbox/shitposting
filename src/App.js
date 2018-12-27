5c8f88ca4f2037df40bf0a0c6a05d2710a191275
taiyoslime <hase.tekitou@gmail.com>
2018-12-27 18:37:39 +0900

    add some tempalate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {data} from './template';
 import './App.css';
 
 class App extends Component {
@@ -7,40 +8,30 @@ class App extends Component {
 		super()
 
 		this.state = {
-			data: [
-				{
-				"title":"誰もお前を愛さない",
-				name:'nobody_loves_you',
-				text:(e => `ちくしょう${e}にしやがった\n${e}はいつもそうだ\nこの${e}はお前の人生そのものだ\nお前はいろいろな${e}に手を付けるが何一つ成し遂げられない\n誰も${e}を愛さない`
-			)},{
-				title:"突然ですが",
-				name:"todays_ohohoho",
-				text:(e => `突然ですが！！！！！！！！！${e}さんは本日のオホホハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハハｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ`)
-			},{
-				title:"昆布",
-				name:"konbu",
-				text:(e => `昆布を食べたらはまって抜け出せなくなった${e}の図
-　｢＼
-　　ヽ )　　　 ／ )
-　　/ /　　　  (　/
-　 / /　　　    ｜｜
-　(　＼　　　｜｜
-　 ＼　＼　 　/ ｜
-　　 ＼　ヽ／　/
-　　＿｜　　　/_＿
-　　       昆布`)
-			}],
-			value : ""
-		}
+			data: data,
+            value : "",
+            error : "",
+        }
 
 	}
 
 	handle (e) {
 		/* eslint-disable */
-		try {
-			window.open('https://twitter.com/intent/tweet?text=' + encodeURI(this.state.data.filter((item, index) => {if(item.name === e.target.id)return true})[0].text((`@${this.state.value}`)).substr(0,140)))
-		} catch (error){
-		}
+
+        let tweet_text = this.state.data.filter((item, index) => {if(item.name === e.target.id)return true})[0].text(`@${this.state.value}`)
+
+        if(tweet_text.length > 140) {
+            this.setState({error:"The lenght of the text is too long"})
+        } else if (this.state.value == ""){
+            this.setState({error:"Username cannot be empty"})
+        } else {
+            this.setState({error:""})
+            try {
+                window.open('https://twitter.com/intent/tweet?text=' + encodeURI(tweet_text))
+            } catch (error){
+            }
+        }
+
 	}
 
 	componentWillMount(){
@@ -63,7 +54,8 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h3> Shitposting </h3>
-				@ <input  type="text" onChange={this.change.bind(this)} value={this.state.value}></input>
+                <div className="Error">{this.state.error}</div>
+				@ <input type="text" onChange={this.change.bind(this)} value={this.state.value}></input>
 				<ul>
 					{this.state.data.map(el => <li>{el.title} <button id={el.name} onClick={this.handle.bind(this)}> Tweet!</button></li>)}
 				</ul>
